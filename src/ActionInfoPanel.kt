7175b5e324c64bdd19a04078032a641d890a87db
nik <Nikolay.Chashnikov@jetbrains.com>
2014-04-02 14:01:43 +0400

    fixed incompatibility with the latest builds

--- src/ActionInfoPanel.kt
+++ src/ActionInfoPanel.kt
@@ -55,7 +55,8 @@ class ActionInfoPanel(project: Project, textFragments: List<Pair<String, Font?>>
 
         hint = with (JBPopupFactory.getInstance()!!.createComponentPopupBuilder(this, this) as ComponentPopupBuilderImpl) {
             setAlpha(1.0.toFloat())
-            setMaskProvider { RoundRectangle2D.Double(1.0, 1.0, it!!.getWidth()-2, it.getHeight()-2, arcSize.toDouble(), arcSize.toDouble()) }
+            //todo[nik] this method was removed from IDEA
+//            setMaskProvider { RoundRectangle2D.Double(1.0, 1.0, it!!.getWidth()-2, it.getHeight()-2, arcSize.toDouble(), arcSize.toDouble()) }
             setFocusable(false)
             setBelongsToGlobalPopupStack(false)
             setCancelKeyEnabled(false)
